2476067cd217ef13a5979fc96613332e256293cd
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-11-22 22:51:55 -0300

    Converte os valores possiveis para Float.

--- treinador_j48.py
+++ treinador_j48.py
@@ -1,80 +1,83 @@
 def ler_dados():
 	dados = []
+	posicoes_flutuantes = [2, 3, 6, 10, 12] #posicoes que tem valores  aserem convertidos para flutuante
 	linhas = open("teste_obesidade.txt").readlines()
 	
 	for i in linhas:
 		dados.append(i.split(","))
 
-	for i in range(len(dados)):
-		x = dados[i][16].split("\n") #eliminando o "\n" do final
-		dados[i][16] = x[0]
-	
+	for i in dados:
+		i[16] = i[16].split("\n")[0] #eliminando o "\n" do final
+		for g in posicoes_flutuantes:
+			i[g] = float(i[g])
+
 	return dados
+
 
 def testar_j48(dados):
 	acerto = 0
 
 	for i in dados:
-		if float(i[3]) <= 99.530971:
-			if float(i[3]) <= 60:
-				if float(i[2]) <= 1.66:
-					if float(i[3]) <= 46.655622:
-						if float(i[2]) <= 1.513202:
+		if i[3] <= 99.530971:
+			if i[3] <= 60:
+				if i[2] <= 1.66:
+					if i[3] <= 46.655622:
+						if i[2] <= 1.513202:
 							obesidade = "Normal_Weight"
 						else:
-							if float(i[3]) <= 44.918255:
+							if i[3] <= 44.918255:
 								obesidade = "Insufficient_Weight"
 							else:
-								if float(i[2]) <= 1.58:
+								if i[2] <= 1.58:
 									obesidade = "Normal_Weight"
 								else:
 									obesidade = "Insufficient_Weight"
 					else:
-						if float(i[2]) <= 1.53777:
+						if i[2] <= 1.53777:
 							if i[5] == "yes":
 								if i[11] == "yes":
 									obesidade = "Overweight_Level_I"
 								else:
-									if float(i[3]) <= 56.307019:
+									if i[3] <= 56.307019:
 										obesidade = "Normal_Weight"
 									else:
 										obesidade = "Overweight_Level_I"
 							else:
 								obesidade = "Normal_Weight"
 						else:
-							if float(i[3]) <= 50.664459:
-								if float(i[2]) <= 1.605404:
+							if i[3] <= 50.664459:
+								if i[2] <= 1.605404:
 									obesidade = "Normal_Weight"
 								else:
 									obesidade = "Insufficient_Weight"
 							else:
 								obesidade = "Normal_Weight"
 				else:
-					if float(i[3]) <= 54.997374:
+					if i[3] <= 54.997374:
 						obesidade = "Insufficient_Weight"
 					else:
-						if float(i[2]) <= 1.750384:
+						if i[2] <= 1.750384:
 							obesidade = "Normal_Weight"
 						else:
-							if float(i[12]) <= 1.330519:
+							if i[12] <= 1.330519:
 								obesidade = "Normal_Weight"
 							else:
 								obesidade = "Insufficient_Weight"
 			else:
-				if float(i[3]) <= 76:
-					if float(i[2]) <= 1.5891:
+				if i[3] <= 76:
+					if i[2] <= 1.5891:
 						if i[5] == "yes":
-							if float(i[3]) <= 70:
+							if i[3] <= 70:
 								obesidade = "Overweight_Level_I"
 							else:
 								obesidade = "Obesity_Type_I"
 						else:
 							obesidade = "Overweight_Level_II"
 					else:
-						if float(i[2]) <= 1.734832:
-							if float(i[3]) <= 72:
-								if float(i[2]) <= 1.648143:
-									if float(i[3]) <= 64.4:
+						if i[2] <= 1.734832:
+							if i[3] <= 72:
+								if i[2] <= 1.648143:
+									if i[3] <= 64.4:
 										obesidade = "Normal_Weight"
 									else:
 										obesidade = "Overweight_Level_I"
@@ -82,7 +85,7 @@ def testar_j48(dados):
 									if float(i[1]) <= 27.474245:
 										obesidade = "Normal_Weight"
 									else:
-										if float(i[6]) <= 2.805533:
+										if i[6] <= 2.805533:
 											obesidade = "Overweight_Level_I"
 										else:
 											obesidade = "Normal_Weight"
@@ -95,8 +98,8 @@ def testar_j48(dados):
 								else:
 									obesidade = "Overweight_Level_I"
 						else:
-							if float(i[2]) <= 1.85:
-								if float(i[3]) <= 75.306702:
+							if i[2] <= 1.85:
+								if i[3] <= 75.306702:
 									obesidade = "Normal_Weight"
 								else:
 									if float(i[1]) <= 28.493397:
@@ -104,38 +107,38 @@ def testar_j48(dados):
 									else:
 										obesidade = "Overweight_Level_I"
 							else:
-								if float(i[3]) <= 65.423942:
+								if i[3] <= 65.423942:
 									obesidade = "Insufficient_Weight"
 								else:
 									obesidade = "Normal_Weight"
 				else:
-					if float(i[2]) <= 1.729177:
-						if float(i[2]) <= 1.64639:
-							if float(i[3]) <= 92:
+					if i[2] <= 1.729177:
+						if i[2] <= 1.64639:
+							if i[3] <= 92:
 								obesidade = "Obesity_Type_I"
 							else:
 								obesidade = "Obesity_Type_II"
 						else:
-							if float(i[3]) <= 85.316199:
-								if float(i[2]) <= 1.665199:
-									if float(i[3]) <= 81.384224:
+							if i[3] <= 85.316199:
+								if i[2] <= 1.665199:
+									if i[3] <= 81.384224:
 										obesidade = "Overweight_Level_II"
 									else:
 										obesidade = "Obesity_Type_I"
 								else:
 									obesidade = "Overweight_Level_II"
 							else:
-								if float(i[3]) <= 88.675503:
-									if float(i[2]) <= 1.694997:
+								if i[3] <= 88.675503:
+									if i[2] <= 1.694997:
 										obesidade = "Obesity_Type_I"
 									else:
 										obesidade = "Overweight_Level_II"
 								else:
 									obesidade = "Obesity_Type_I"
 					else:
-						if float(i[3]) <= 91:
-							if float(i[2]) <= 1.799779:
-								if float(i[3]) <= 83.26312:
+						if i[3] <= 91:
+							if i[2] <= 1.799779:
+								if i[3] <= 83.26312:
 									if float(i[1]) <= 30.163408:
 										obesidade = "Overweight_Level_I"
 									else:
@@ -146,7 +149,7 @@ def testar_j48(dados):
 									else:
 										obesidade = "Overweight_Level_I"
 							else:
-								if float(i[3]) <= 84.49798:
+								if i[3] <= 84.49798:
 									obesidade = "Normal_Weight"
 								else:
 									if i[8] == "no":
@@ -158,7 +161,7 @@ def testar_j48(dados):
 									if i[8] == "Always":
 										obesidade = "Overweight_Level_I"
 						else:
-							if float(i[2]) <= 1.794827:
+							if i[2] <= 1.794827:
 								obesidade = "Obesity_Type_I"
 							else:
 								obesidade = "Overweight_Lavel_II"
@@ -167,22 +170,22 @@ def testar_j48(dados):
 				obesidade = "Obesity_Type_III"
 			else:
 				if float(i[1]) <= 22.524036:
-					if float(i[6]) <= 2.571274:
+					if i[6] <= 2.571274:
 						obesidade = "Obesity_Type_I"
 					else:
-						if float(i[12]) <= 0.967627:
+						if i[12] <= 0.967627:
 							obesidade = "Obesity_Type_II"
 						else:
 							obesidade = "Obesity_Type_I"
 				else:
-					if float(i[3]) <= 109.599453:
-						if float(i[2]) <= 1.750384:
-							if float(i[10]) <= 1.768111:
+					if i[3] <= 109.599453:
+						if i[2] <= 1.750384:
+							if i[10] <= 1.768111:
 								obesidade = "Obesity_Type_II"
 							else:
 								obesidade = "Obesity_Type_I"
 						else:
-							if float(i[2]) <= 1.86516:
+							if i[2] <= 1.86516:
 								obesidade = "Obesity_Type_I"
 							else:
 								obesidade = "Overweight_Level_II"
